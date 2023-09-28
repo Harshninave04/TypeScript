a4d2b44c4fce3871dfbd6bb7c312c8b49452f21b
Harsh Ninave <harshninave58@gmail.com>
2023-09-28 19:35:56 +0530

    Arithmetic practice

---
+++ Typescript/arithmetic.ts
@@ -0,0 +1,23 @@
+const x = 10;
+const y = 4;
+
+const add = x + y;
+add;
+
+const subs = x - y;
+subs;
+
+const mul = x * y;
+mul;
+
+const division = x / y;
+division;
+
+const modulo = x % y;
+modulo;
+
+const exponent = x ** y;
+exponent;
+
+const bitwiseXOR = x ^ y;
+bitwiseXOR;

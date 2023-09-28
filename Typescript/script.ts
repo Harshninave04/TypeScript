ff90011a251b89962fc2e15cd3481ee73a076327
Harsh Ninave <harshninave58@gmail.com>
2023-09-28 19:37:12 +0530

    Typescript practice folder

---
+++ Typescript/script.ts
@@ -0,0 +1,18 @@
+// union type : variable can be of multiple type
+
+let nonInitialized: number | undefined | null | boolean | String;
+nonInitialized = 34;
+nonInitialized = "dfasf";
+
+// Declare a variable with the type of string
+
+let a = 20;
+let b = "harsh is here";
+const c: String = "sdf";
+
+const d: boolean = true;
+
+// Declare a variable which can be either string or number
+// It can be string or number
+const strnum: String | number = "String";
+const strnum1: String | number = 32;

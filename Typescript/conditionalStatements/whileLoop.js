32d974bcd77aa812f66ae0c42c8b7715f2b8743a
Harsh Ninave <harshninave58@gmail.com>
2023-09-30 23:12:36 +0530

    while loop

---
+++ Typescript/conditionalStatements/whileLoop.js
@@ -0,0 +1,14 @@
+function vichitra() {
+  let num = 1;
+  while (num <= 10) {
+    console.log(num);
+    num++;
+    if (num === 6) {
+      console.log(num);
+      console.log("I found it");
+      break;
+    }
+  }
+}
+
+vichitra();

bcfe22d83fe201eab00f7fd5cd76343cf4bc0b39
Harsh Ninave <harshninave58@gmail.com>
2023-09-30 23:48:38 +0530

    do while loop

---
+++ Typescript/conditionalStatements/doWhileLoop.js
@@ -0,0 +1,13 @@
+let trial = 1;
+do {
+  console.log(trial);
+  trial++;
+} while (trial < 2);
+
+// Write a do while loop that logs number between 1 to 10 in reverse order
+
+let counter = 10;
+do {
+  console.log(counter);
+  counter--;
+} while (counter >= 1);

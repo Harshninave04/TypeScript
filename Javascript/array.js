3bc1bcc215d5576d39f37e91f08e72d35d9d7f9b
Harsh Ninave <harshninave58@gmail.com>
2023-10-03 00:44:03 +0530

    JS array

--- Javascript/array.js
+++ Javascript/array.js
@@ -3,10 +3,10 @@ let arr = [1, 2, 3, 4, 5, 6];
 console.log(arr[0]);
 console.log(arr.length);
 for (let i = 0; i < arr.length; i++) {
-    if (i === 5) {
-        arr.push(7);
-    }
-    console.log(arr[i]);
+  if (i === 5) {
+    arr.push(7);
+  }
+  console.log(arr[i]);
 }
 arr.push(8); // push adds number in the end
 console.log(arr);
